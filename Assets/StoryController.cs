3abb474bd68781d01a21f7a7e823f83c02ccb2fc
Eric <charmjunewonder@gmail.com>
2014-09-22 18:23:32 -0400

    add story scene

---
+++ Assets/StoryController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoryController : MonoBehaviour {
+	public GameObject storyBoard;
+	public Texture[] storyPics;
+	// Use this for initialization
+	void Start () {
+
+		//StartCoroutine("changeStoryPic");
+	}
+	
+	IEnumerator changeStoryPic(){
+		for(int i = 0; i < 9; ++i){
+			storyBoard.guiTexture.texture = storyPics [i];
+			yield return new WaitForSeconds(2.0f);
+		}
+	}
+}

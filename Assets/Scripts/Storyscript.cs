a6b4628b76abff4e1d796adfdb2ef8227ce08bbc
wertkh32 <virtwert@hotmail.com>
2014-09-25 13:08:45 -0400

    small bug

--- Assets/Scripts/Storyscript.cs
+++ Assets/Scripts/Storyscript.cs
@@ -1,7 +1,7 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class Storyscript : MonoBehaviour {
+public class StoryScript : MonoBehaviour {
 	MovieTexture mov;
 
 	// Use this for initialization

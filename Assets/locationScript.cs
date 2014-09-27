fa2f3f9e3d71713d5d92b0dfe5abd145d541da3f
wertkh32 <virtwert@hotmail.com>
2014-09-26 23:03:46 -0400

    locationScript

--- Assets/locationScript.cs
+++ Assets/locationScript.cs
@@ -8,7 +8,9 @@ public class locationScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		Vector3 pos = this.transform.localPosition;
+		pos.z = -8.0f;
+		this.transform.localPosition = pos;
 	}
 
 	public void SendText(int texnum)
@@ -16,12 +18,56 @@ public class locationScript : MonoBehaviour {
 		StartCoroutine (WaitAndSend(texnum));
 	}
 
+	IEnumerator FadeIn()
+	{
+		for (float f=0; f<1.0f; f+= Time.deltaTime) 
+		{
+			yield return null;
+			Vector3 pos = this.transform.localPosition;
+			pos.z += 4.0f * Time.deltaTime;
+			this.transform.localPosition = pos;
+			//Color c = this.renderer.material.color;
+			//c.a = Mathf.Clamp01(f);
+			//this.renderer.material.color = c;
+			//this.transform.localPosition;
+
+		}
+
+		Vector3 pos2 = this.transform.localPosition;
+		pos2.z = -4.0f;
+		this.transform.localPosition = pos2;
+	}
+
+	IEnumerator FadeOut()
+	{
+		for (float f=0; f<1.0f; f+= Time.deltaTime) 
+		{
+			yield return null;
+			Vector3 pos = this.transform.localPosition;
+			pos.z -= 4.0f * Time.deltaTime;
+			this.transform.localPosition = pos;
+			//Color c = this.renderer.material.color;
+			//c.a = Mathf.Clamp01(f);
+			//this.renderer.material.color = c;
+			//this.transform.localPosition;
+			
+		}
+
+		Vector3 pos2 = this.transform.localPosition;
+		pos2.z = -8.0f;
+		this.transform.localPosition = pos2;
+	}
+
+
 	IEnumerator WaitAndSend(int texnum)
 	{
 		yield return new WaitForSeconds(2.0f);
 		this.renderer.material.mainTexture = locationtex [texnum];
+		StartCoroutine (FadeIn ());
 		audio.clip = clips [texnum];
 		audio.Play ();
+		yield return new WaitForSeconds(5.0f);
+		StartCoroutine (FadeOut ());
 	}
 
 	// Update is called once per frame
